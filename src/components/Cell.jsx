9ae3a64075b424a15ef15b806dc44d2110ea9efb
priyank rai <0priyankrai@gmail.com>
2023-05-30 18:12:49 +0530

    finishing project

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,13 +1,50 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
-export default function Cell({ cell, turn, setTurn }) {
+export default function Cell({
+  cell,
+  turn,
+  setTurn,
+  boardState,
+  row,
+  col,
+  setBoardState,
+}) {
   const [isDisabled, setIsDisabled] = useState(false);
   const [cellValue, setcellValue] = useState("#");
+
+  let checkWin = () => {
+    let i = (j = 0);
+    for (i = 0; i < 2; i++) {
+      for (j = 0; j < 2; j++) {
+        if (boardState[i][j] == undefined && (i != 0 || j != 0)) continue;
+
+        // check for diagonals
+        if (
+          i == 0 &&
+          j == 0 &&
+          (boardState[i][j] == boardState[i + 1][j + 1]) ==
+            boardState[i + 2][j + 2]
+        ) {
+          console.log("You Won ", turn);
+        }
+        if (
+          i == 0 &&
+          j == 2 &&
+          (boardState[i][j] == boardState[i + 1][j - 1]) ==
+            boardState[i + 2][j - 2]
+        ) {
+          console.log("You Won ", turn);
+        }
+      }
+    }
+  };
+
   let handleClick = () => {
-      cell.value = turn;
-    
+    const updatedBoardState = [...boardState];
+    updatedBoardState[row][col] = turn;
+
     setIsDisabled(true);
 
     if (turn == "X") {
@@ -17,10 +54,15 @@ export default function Cell({ cell, turn, setTurn }) {
       setcellValue("O");
       setTurn("X");
     }
+    setBoardState(updatedBoardState);
+    checkWin();
+    console.log(row, col);
+
+    console.log(boardState);
   };
   return (
     <TouchableOpacity
-      className="border  p-10"
+      className="border p-8 "
       onPress={handleClick}
       disabled={isDisabled}
     >
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,26 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Cell from "./components/Cell";
 
-const Index = () => {
-  let cell = {
-    value: undefined,
-    color: "yellow,",
-  };
-  let boardState = [];
-  for (let i = 0; i <= 2; i++) {
-    const row = [];
-    for (let j = 0; j <= 2; j++) row.push(cell);
-    boardState.push(row);
-  }
+// Making Board
 
+const createEmptyRow = () => {
+  const row = [];
+  for (let j = 0; j <= 2; j++) {
+    row.push(undefined);
+  }
+  return row;
+};
+
+let _boardState = [];
+for (let i = 0; i <= 2; i++) {
+  _boardState.push(createEmptyRow());
+}
+
+const Index = () => {
+  const [boardState, setBoardState] = useState(_boardState);
   const [turn, setTurn] = useState("X");
+
   return (
     <View className="flex flex-1 justify-center items-center m-5">
       <Text>current turn: {turn}</Text>
@@ -28,6 +34,10 @@ const Index = () => {
                 cell={cell}
                 turn={turn}
                 setTurn={setTurn}
+                boardState={boardState}
+                row={rowIndex}
+                col={columnIndex}
+                setBoardState={setBoardState}
               />
             ))}
           </View>
